425641b3ee7ba269e412c436d6b7df8adc891002
msweeneydev <mail@msweeneydev.com>
2019-06-27 12:23:10 +0100

    minor tweaks

--- api/index.js
+++ api/index.js
@@ -3,18 +3,23 @@ const getParams = require('../lib/getParams');
 const getData = require('../lib/getData');
 const deleteData = require('../lib/deleteData');
 
+let data = db;
+
 module.exports = (req, res) => {
   let params = getParams(req);
-  let data = db;
   console.log('METHOD', req.method);
   if (params.length === 0) res.json({ error: 'Empty request.' });
   switch (req.method) {
     case 'GET':
       data = getData(db, params);
+      console.log('GET DATA', data);
+      console.log('GET DB', db);
       res.send(data);
       break;
     case 'DELETE':
       data = deleteData(db, params);
+      console.log('DELETE DATA', data);
+      console.log('DELETE DB', db);
       res.send(data);
       break;
   }
